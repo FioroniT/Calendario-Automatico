e812cb2a1d9052ec129b52f8a273e6d1b42d5252
Nyx <elerii098@gmail.com>
2025-05-29 20:35:40 -0300

    Mejora agregada al front y migracion a flask

---
+++ Frontend/backend/calendauto/static/js/create-carreras-btn.js
@@ -0,0 +1,16 @@
+
+const container = document.getElementById('dpdw-carreras');
+
+var formDiv = document.getElementById('materias-content');
+for (let i = 0; i < carreras_obj.carreras.length; i++) {
+  const button = document.createElement('button');
+  button.innerHTML = carreras_obj.carreras[i].nombre;
+  button.addEventListener('click', function() {
+    changeCarrera(carreras_obj.carreras[i].nombre);
+    formDiv.innerHTML = '';
+    generateMaterias(i);
+    showMaterias();
+    dropDownCarreras();
+  });
+  container.appendChild(button);
+}
